ae58a730cf0e8ebfc50d1cd5a02670e9890bde46
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-31 17:09:27 +0100

    seventh commit

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -47,8 +47,8 @@ const Details = () => {
             Calories: {Math.round(recipeDet.calories)}
           </p>
           {recipeDet.digest.slice(0, 4).map((item, index) => (
-            <p data-test={item.label+"test"} key={index}>
-              {item.label}  : {Math.round(item.total)}
+            <p data-test={item.label + "test"} key={index}>
+              {item.label} : {Math.round(item.total)}
             </p>
           ))}
         </OtherPart>
